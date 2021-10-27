71f0f8458665cc18ee5511ba8e68e5d12bed0fd1
ShockIgorek <shokin1996@yandex.ru>
2021-10-28 02:43:21 +0700

    start styles

---
+++ scripts/slider.js
@@ -0,0 +1,10 @@
+const slider = document.querySelector('.slider');
+const sliderItem = slider.querySelector('.slider__item')
+const leftButton = slider.querySelector('.scroll__button_left');
+const rightButton = slider.querySelector('.scroll__button_right');
+let move = 0;
+
+
+
+
+
